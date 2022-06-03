ee21933bbeefde4ff39149ec72b80c657f515913
Sean Zheng <blackhorseya@gmail.com>
2022-06-03 19:11:58 +0800

    fix: stop pick at string

--- src/components/booking/BookingList.js
+++ src/components/booking/BookingList.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import {
-  Grid,
-  IconButton,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Paper,
-  Typography,
+    Grid, IconButton, List, ListItem, ListItemIcon, ListItemText, Paper, Typography,
 } from '@material-ui/core';
 import {connect} from 'react-redux';
 import {bookingActions} from '../../_actions';
@@ -15,112 +8,96 @@ import {Cancel, Refresh, Room} from '@material-ui/icons';
 import moment from 'moment';
 
 class BookingList extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.handleOpenMaps = this.handleOpenMaps.bind(this);
-    this.handleCancelBooking = this.handleCancelBooking.bind(this);
-    this.handleList = this.handleList.bind(this);
-  }
-
-  componentDidMount() {
-    const token = localStorage.getItem('token');
-
-    if (token !== null) {
-      this.props.list(token);
-    }
-  }
-
-  handleList() {
-    const token = localStorage.getItem('token');
-
-    if (token !== null) {
-      this.props.list(token);
-    }
-  }
-
-  handleOpenMaps(lat, long) {
-    const url = `https://www.google.com/maps/search/?api=1&query=${lat},${long}`;
-    window.open(url, '_blank');
-  }
-
-  handleCancelBooking(id) {
-    const token = localStorage.getItem('token');
-
-    if (token !== null) {
-      this.props.cancel(id, token);
-    }
-  }
-
-  render() {
-    const {booking} = this.props;
-
-    function formatTime(s) {
-      return moment(new Date(s / 1000 / 1000)).format('YYYY-MM-DD HH:mm');
+        this.handleOpenMaps = this.handleOpenMaps.bind(this);
+        this.handleCancelBooking = this.handleCancelBooking.bind(this);
+        this.handleList = this.handleList.bind(this);
     }
 
-    return (
-        <Paper style={{padding: 20}}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Grid container spacing={2}>
-                <Grid item>
-                  <Typography variant={'h5'}>Booking List</Typography>
-                </Grid>
-                <Grid item>
-                  <IconButton onClick={() => this.handleList()}>
-                    <Refresh/>
-                  </IconButton>
-                </Grid>
-              </Grid>
-              {booking.loading && (
-                  <Typography>Loading...</Typography>
-              )}
-              {booking.loading === false && (
-                  booking.error && (
-                      <Typography>{booking.error}</Typography>
-                  )
-              )}
-              {booking.loading === false && booking.data && (
-                  <List>
-                    {booking.data.map((item, _) =>
-                        <ListItem key={item.no}>
-                          <ListItemText id={item.no} primary={item.car_id}/>
-                          <ListItemText id={item.no}
-                                        primary={formatTime(
-                                            item.stop_pick_at)}/>
-                          <ListItemIcon>
-                            <IconButton onClick={() => this.handleOpenMaps(
-                                item.car_latitude, item.car_longitude)}>
-                              <Room/>
+    componentDidMount() {
+        const token = localStorage.getItem('token');
+
+        if (token !== null) {
+            this.props.list(token);
+        }
+    }
+
+    handleList() {
+        const token = localStorage.getItem('token');
+
+        if (token !== null) {
+            this.props.list(token);
+        }
+    }
+
+    handleOpenMaps(lat, long) {
+        const url = `https://www.google.com/maps/search/?api=1&query=${lat},${long}`;
+        window.open(url, '_blank');
+    }
+
+    handleCancelBooking(id) {
+        const token = localStorage.getItem('token');
+
+        if (token !== null) {
+            this.props.cancel(id, token);
+        }
+    }
+
+    render() {
+        const {booking} = this.props;
+
+        function formatTime(s) {
+            return moment(new Date(s / 1000 / 1000)).format('YYYY-MM-DD HH:mm');
+        }
+
+        return (<Paper style={{padding: 20}}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Grid container spacing={2}>
+                        <Grid item>
+                            <Typography variant={'h5'}>Booking List</Typography>
+                        </Grid>
+                        <Grid item>
+                            <IconButton onClick={() => this.handleList()}>
+                                <Refresh/>
                             </IconButton>
-                          </ListItemIcon>
-                          <ListItemIcon>
-                            <IconButton onClick={() => this.handleCancelBooking(
-                                item.no)}>
-                              <Cancel/>
-                            </IconButton>
-                          </ListItemIcon>
+                        </Grid>
+                    </Grid>
+                    {booking.loading && (<Typography>Loading...</Typography>)}
+                    {booking.loading === false && (booking.error && (<Typography>{booking.error}</Typography>))}
+                    {booking.loading === false && booking.data && (<List>
+                        {booking.data.map((item, _) => <ListItem key={item.no}>
+                            <ListItemText id={item.no} primary={item.car_id}/>
+                            <ListItemText id={item.no} primary={item.stop_pick_at}/>
+                            <ListItemIcon>
+                                <IconButton
+                                    onClick={() => this.handleOpenMaps(item.car_latitude, item.car_longitude)}>
+                                    <Room/>
+                                </IconButton>
+                            </ListItemIcon>
+                            <ListItemIcon>
+                                <IconButton onClick={() => this.handleCancelBooking(item.no)}>
+                                    <Cancel/>
+                                </IconButton>
+                            </ListItemIcon>
                         </ListItem>)}
-                  </List>
-              )}
+                    </List>)}
+                </Grid>
             </Grid>
-          </Grid>
-        </Paper>
-    );
-  }
+        </Paper>);
+    }
 }
 
 function mapStateToProps(state) {
-  const {booking} = state;
-  return {booking};
+    const {booking} = state;
+    return {booking};
 }
 
 const actionCreators = {
-  list: bookingActions.list,
-  cancel: bookingActions.cancel,
+    list: bookingActions.list, cancel: bookingActions.cancel,
 };
 
-const connectedBookingList = connect(mapStateToProps, actionCreators)(
-    BookingList);
+const connectedBookingList = connect(mapStateToProps, actionCreators)(BookingList);
 export {connectedBookingList as BookingList};
